8168271484255227ef1550ba1991ce60e63111b6
Felipe Jara <fjararibet@gmail.com>
2026-05-17 15:46:23 -0400

    rebrand to Pulso Urbano

--- scripts/print-preview-url.sh
+++ scripts/print-preview-url.sh
@@ -5,5 +5,5 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 sanitized=$(echo "$branch" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g; s/^-*//; s/-*$//')
 
 echo ""
-echo "Preview URL: https://${sanitized}-esgrima.fjararibet.workers.dev"
+echo "Preview URL: https://${sanitized}-pulso-urbano.fjararibet.workers.dev"
 echo ""
